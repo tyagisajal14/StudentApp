134df71eefa8058775ff90b0c97c95d69453d0e4
tyagisajal14 <70626619+tyagisajal14@users.noreply.github.com>
2020-09-07 09:52:23 +0530

    Add files via upload

---
+++ StudentApp_Services/Interfaces/IStudentResultService.cs
@@ -0,0 +1,16 @@
+﻿using StudentApp_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp_Services.Interfaces
+{
+    public interface IStudentResultService
+    {
+        bool AddStudentResult(StudentResults Student);
+        bool UpdateStudentResult(StudentResults Student);
+        bool DeleteStudentResult(string StudentResultId);
+        List<StudentDetails> GetStudentsResultByStuId(string StudentId);
+        StudentResults GetStudentResultByResultId(string resultId);
+    }
+}
---
+++ StudentApp_Services/Interfaces/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using StudentApp_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp_Services.Interfaces
+{
+    public interface IStudentService
+    {
+        Students AddStudent(Students Student);
+        bool UpdateStudent(Students Student);
+        bool DeleteStudent(string StudentId);
+        List<Students> GetAllStudents();
+        Students GetStudentById(string StudentId);
+    }
+}
