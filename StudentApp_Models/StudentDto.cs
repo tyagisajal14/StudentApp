df9d4a42c27519a9e359322e603903beb8eb42c1
tyagisajal14 <70626619+tyagisajal14@users.noreply.github.com>
2020-09-07 09:51:32 +0530

    Add files via upload

---
+++ StudentApp_Models/StudentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp_Models
+{
+    public class StudentDto
+    {
+        public long StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Class { get; set; }
+    }
+}
---
+++ StudentApp_Models/StudentResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp_Models
+{
+    public class StudentResultDto
+    {
+        public long ResultId { get; set; }
+        public long? StudentId { get; set; }
+        public string SubjectName { get; set; }
+        public int? Marks { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Class { get; set; }
+    }
+}
